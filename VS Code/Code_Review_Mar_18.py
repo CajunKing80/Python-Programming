8593af70b0bd8d4c6840d0ef5d249639d6bd69ed
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-03-12 15:37:02 -0500

    Create Code_Review_Mar_18.py

---
+++ VS Code/Code_Review_Mar_18.py
@@ -0,0 +1,60 @@
+# =================================================================================================
+# EASY ============================================================================================
+# =================================================================================================
+
+
+# def pling_plang():
+
+#     '''Pling Plang is a function used to return the output  of intergers divisible by 3, 5, and 7'''
+
+#     for i in range(1,101):
+#         if i % 3 == 0:
+#             print("Pling")
+#         elif i % 5 == 0:
+#             print("Plang")
+#         elif i % 7 == 0:
+#             print("Plong")
+#         else:
+#             print(i)
+
+# pling_plang()
+
+
+# =================================================================================================
+# MEDIUM ==========================================================================================
+# =================================================================================================
+
+
+# Create the function
+# def pling_plang():
+
+#     # Define the variable "my_dictionary" as a dictionary with empty lists as placeholders for the values. 
+#     my_dictionary = {
+#         "Pling": [],
+#         "Plang": [],
+#         "Plong": [],
+#         "No_match": []
+#     }
+
+#     # Logic to determine "divisible by". If true, append (i) to the corresponding key in my_dictionary.
+#     for i in range(1,101):
+#         if i % 3 == 0:
+#             my_dictionary["Pling"].append(i)
+#         elif i % 5 == 0:
+#             my_dictionary["Plang"].append(i)
+#         elif i % 7 == 0:
+#             my_dictionary["Plong"].append(i)
+#         else:
+#             my_dictionary["No_match"].append(i)
+
+#     for key in my_dictionary:
+#         print()
+#         print(key, ":", my_dictionary[key])
+
+# pling_plang()
+
+
+# =================================================================================================
+# HARD ============================================================================================
+# =================================================================================================
+
