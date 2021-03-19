6b027f8f579eb1e37d8b12852df093ec15d2c666
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-03-19 17:58:53 -0400

    New exercism file

---
+++ EXERCISM Exercises/Two_Fer.py
@@ -0,0 +1,20 @@
+def two_fer():
+    
+    while True: 
+
+        userName = 'Zaphod'
+
+        if userName == 'Alice':
+            print ('One for Alice, one for me.')
+            break
+        elif userName == 'Bob':
+            print ('One for Bob, one for me.')
+            break 
+        elif userName == ():
+            print ('One for you, one for me.')
+            break
+        elif userName == 'Zaphod':
+            print ('One for Zaphod, one for me.')
+            break
+
+two_fer()  
