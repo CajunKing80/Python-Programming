0933921eb7e2b32b94f6f4042a7dc522ca061fb3
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-03-20 14:36:17 -0400

    Update Code_Review_Mar_25.py

--- VS Code/Code Review/Code_Review_Mar_25.py
+++ VS Code/Code Review/Code_Review_Mar_25.py
@@ -8,21 +8,24 @@
 # Format current time using string formatting
 # currentTime = now.strftime("%H:%M:%S")
 # currentTime = strftime("%H:%M:%S", localtime())
+# from time import localtime, strftime
+# from datetime import datetime, timezone
+# now = datetime.now()
+
+import tkinter as tk
+import time as tm
 
 
-from time import localtime, strftime
-from datetime import datetime, timezone
+def display_time():
 
+    current_time = tm.strftime('%H:%M:%S') 
+    clock_label['text'] = current_time
+    my_window.after (1000,display_time)
 
-now = datetime.now()
-
-# def timeLoop(currentTime):
-
-#     for everySecond in currentTime:
-#         print(currentTime)
-#         currentTime = currentTime + localtime
-
-# timeLoop(currentTime)
-
-
-print(now.strftime("%m-%d-%Y %H:%M:%S"))
+# Format Windows window to display the current time
+my_window = tk.Tk() #Create an instance of a window
+my_window.title('Current Time') # Set title of window
+clock_label = tk.Label(my_window, font = 'ariel 100', bg = 'black', fg = 'red') # Format the window's font, size, background and foreground
+clock_label.grid (row = 0, column = 0) # Sets label to label position 
+display_time()
+my_window.mainloop() # Event loop
