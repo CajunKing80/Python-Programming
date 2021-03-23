cce717150fd8927de6f091be04a31860e8cc3105
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-03-23 18:47:30 -0400

    Update pangram.py

--- EXERCISM Exercises/pangram.py
+++ EXERCISM Exercises/pangram.py
@@ -1,14 +1,16 @@
-import string
+def is_pangram(sentence):
+    
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    letters_in_sentence = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-def is_pangram():
+    for letter in sentence.lower(): 
+        if letter in alphabet:
+            letters_in_sentence[alphabet.index(letter)] += 1
 
-    var = input()
-
-    for character in var: 
-        if character not in var:
+    for number in letters_in_sentence:
+        if number == 0:
             return False
-        else: 
-            return True
+    return True    
 
 is_pangram()
-print(is_pangram)
