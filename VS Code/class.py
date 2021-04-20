0baa47f3e338a70937e38038bb408b99e42b6b6b
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-04-20 14:52:58 -0400

    Create class.py

---
+++ VS Code/class.py
@@ -0,0 +1,18 @@
+
+def get_average():  
+
+    
+    count = 1
+
+    while count >= 5:
+        grade = int(input())
+        count += 1
+
+    average = grade // 5
+print(average)
+get_average()
+
+# if average >= 60:
+# 	print('This average is passing')
+# else:
+# 	print('This average is failing')
