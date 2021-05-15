8a1066309d7c5f857ccb76643ba1a7900c8a3619
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-05-14 23:21:49 -0400

    First Last update

--- Python Workouts/Exercise 9 First Last/Fisrt Last.py
+++ Python Workouts/Exercise 9 First Last/Fisrt Last.py
@@ -3,7 +3,7 @@ from ipaddress import IPv4Network, IPv4Address
 
 def firstlast(subnet):
 
-    subnet = IPv4Network()
+    subnet = IPv4Network('10.0.0.0/8')
     print(str(subnet.network_address))
 
 firstlast('192.168.0.0/24')
