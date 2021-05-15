71186837b3177d808991519af7d0ab20c8b0d5eb
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-05-15 12:29:00 -0400

    Code Review update

--- VS Code/Code Review/Code_Reveiw_May_20.py
+++ VS Code/Code Review/Code_Reveiw_May_20.py
@@ -19,6 +19,18 @@ print (net4.netmask)
 print (net4.num_addresses)
 print (len(all_hosts))
 print ()
+print (net4.hosts())
+
+
+def firstlast(subnet):
+
+    subnet = ipaddress.ip_network(subnet)
+    network = str(subnet.network_address) + '\n' + str(subnet.broadcast_address)
+    print ()
+    print(network)
+    print()
+ 
+firstlast('192.168.0.0/24')
 
 
 # =================================================================================================
@@ -28,15 +40,14 @@ print ()
 # From the easy challenge, write a script to subnet your network
 # Return whether your network is Public or Private
 
-from ipaddress import ip_network, IPv4Network
+from ipaddress import IPv4Network
 
 
-net4 = IPv4Network('192.169.0.0/24')
-print ()
-print(list(net4.subnets(new_prefix=27)))
-print ()
+network = IPv4Network('192.169.0.0/24')
+for net in network.subnets(new_prefix=27):
+    print(net)
 
-if net4.is_private == True:
-    print(f'{net4} is a private network')
+if network.is_private == True:
+    print(f'\n{network} is a Private Network\n')
 else: 
-    print(f'{net4} is a public network')
+    print(f'\n{network} is a Public Network\n')
