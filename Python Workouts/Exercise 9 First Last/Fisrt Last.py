95417976748ff01ea93b4f911d3db152b3478bbf
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-05-15 11:57:39 -0400

    FirstLast updates

--- Python Workouts/Exercise 9 First Last/Fisrt Last.py
+++ Python Workouts/Exercise 9 First Last/Fisrt Last.py
@@ -4,6 +4,8 @@ def firstlast(subnet):
 
     subnet = ipaddress.ip_network(subnet)
     network = str(subnet.network_address) + '\n' + str(subnet.broadcast_address)
+    print ()
     print(network)
+    print()
  
 firstlast('192.168.0.0/24')
