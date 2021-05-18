e2255e3e72046796b98bd4ccf2905607bc505cdf
CajunKing80 <jeremy.king029@gmail.com>
2021-05-18 10:02:27 -0400

    Commit Automate Video 30

---
+++ Automate the Boring Stuff/Section 11 Files/Video 30 Filenames and File Paths.py
@@ -0,0 +1,24 @@
+import os
+
+os.path.join('folder1', 'folder2', 'folder3', 'folder.png')
+
+os.getcwd()
+
+os.chdir('C:\\')
+
+os.path.abspath('filename') # Returns absolute file path of file
+
+os.path.isabs('filename')
+os.path.relpath('filename')
+
+os.path.dirname()
+os.path.basename()
+
+os.path.exists()
+os.path.isfile()
+os.path.isdir()
+
+os.path.getsize()
+os.listdir()
+
+os.makedirs()
