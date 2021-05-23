8ec4f4090b10d691ad8b1dce4cdf7aa156ac1fa1
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-05-23 18:17:01 -0400

    Code Review commit

--- VS Code/Code Review/Code_Review_May_27.py
+++ VS Code/Code Review/Code_Review_May_27.py
@@ -2,6 +2,8 @@
 # EASY ============================================================================================
 # =================================================================================================
 
+
+print('\n===================================================== EASY =======================================================================')
 alphabet = []
 
 for letter in 'abcdefghijklmnopqrstuvwxyz': 
@@ -10,6 +12,7 @@ print(alphabet)
 
 alphabet = [letter for letter in 'abcdefghijklmnopqrstuvwxyz']
 print (alphabet)
+print('==================================================================================================================================\n')
 
 # =================================================================================================
 # MEDIUM ==========================================================================================
@@ -17,142 +20,58 @@ print (alphabet)
 
 # Conditional List Comprehension
 
+print('\n======= MEDIUM: CONDITIONAL =======')
+number = []
+
+for num in range(20):
+    if num % 2 != 0:
+        number.append(num)
+print(number)
+
 number_list = [number for number in range(20) if number % 2 != 0]
 print (number_list)
+print('======= MEDIUM: CONDITIONAL =======\n')
 
 # Nested List Comprehension
 
+print('\n============ MEDIUM: NESTED ============')
 numbers = []
 for number in range(100):
-    if number % 2 == 0 and number % 5 == 0:
-        numbers.append(number)
+    if number % 2 == 0:
+        if number % 5 == 0:
+            numbers.append(number)
 print (numbers) 
 
 listOfNumbers = [number for number in range(100) if number % 2 == 0 if number % 5 == 0]
 print (listOfNumbers)
+print('============ MEDIUM: NESTED ============\n')
 
 # =================================================================================================
 # HARD ============================================================================================
 # =================================================================================================
 
-vowels = ["a","e","i","o","u"]
-filename = "untitled-1.txt"
-words = [word for word in open(filename, "r").read().split()]
-multivowels = [each for each in open(filename, "r").read().split() if sum(letter in vowels for letter in each) >= 2]
+print('\n===================================== HARD =====================================')
 
+perfect = []
+abundant = []
+deficient = []
 
+for number in range(29): 
+       
+    aliquot_sum = sum([x for x in range(1,number) if number % x == 0])
 
+    if aliquot_sum == number:
+        perfect.append(number)
+    elif aliquot_sum > number:
+        abundant.append(number)
+    elif aliquot_sum < number: 
+        deficient.append(number)
 
+print (perfect)
+print (abundant)
+print (deficient)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from ansible.module_utils.basic import AnsibleModule
-
-# if __name__ == '__main__':
-#     main()
-
-# def main():
-#     data = {
-#         "host" : {
-#             "default" : "localhost", 
-#             "type" : "str"
-#         },
-#         "username" : {
-#             "default" : "username", 
-#             "type" : "str"
-#         },
-#         "password" : {
-#             "default" : "password", 
-#             "type" : "str"
-#         },
-#         "url" : {
-#             "default" : "localhost", 
-#             "type" : "str"
-#         }
-#     }
-
-#     module = AnsibleModule(argument_spec = data)
-
-# host = module.params.get('host')
-# username = module.params.get('username')
-# password = module.params.get('password')
-# url = 'http://' + host + '/authentication'
-# module.params.update({"url": url})
-
-# module.exit_json(changed=True, meta=module.params)
-
-
-# import requests
-# import pygeoip
-# from geolite2 import geolite2
-# import socket
-# import argparse
-# import json
-
-# parser = argparse.ArgumentParser(description='Get IP Geolocation Info')
-# parser.add_argument('--hostname', action="store", dest="hostname", required=True)
-
-# given_args= parser.parse_args()
-# hostname = given_args.hostname
-# ip_address = socket.gethostbyname(hostname)
-
-# print ("IP Address: {0}".format(ip_address))
-
-# reader = geolite2.reader()
-# response = reader.get(ip_address)
-# print (json.dumps(response,indent=4))
-# print (json.dumps(response['continent']['names']['en'],indent=4))
-# print (json.dumps(response['country']['names']['en'],indent=4))
-# print (json.dumps(response['location']['latitude'],indent=4))
-# print (json.dumps(response['location']['longitude'],indent=4))
-# print (json.dumps(response['location']['time_zone'],indent=4))
-
-# ipaddr = requests.get('https://api.ipify.org').text
-# gip = pygeoip.GeoIP(geolite2)
-# res = gip.record_by_addr(ipaddr)
-
-# print (res)
-
-# class IPtoGeo(object):
-#     def __init__(self,ip_address):
-#         self.ip_address = ip_address
-#         self.country = ''
-#         self.state = ''
-#         self.city = ''
-#         self.latitude = ''
-#         self.longitude = ''
-#         # self._get_location()
-
-# def _get_location(self):
-#     json_request = requests.get('https://hackertarget.com/geoip-ip-location-lookup/' % self.ip_address).json()
-#     if 'country_name' in json_request:
-#         self.country = json_request['country_name']
-#     if 'country_code' in json_request:
-#         self.country_code = json_request['country_code']
-#     if 'city' in json_request:
-#         self.city = json_request['city']
-#     if 'latitude' in json_request:
-#         self.latitude = json_request['latitude']
-#     if 'longitude' in json_request:
-#         self.longitude = json_request['longitude']
-
-# if __name__ == '__main__':
-#     geolocation = IPtoGeo('65.191.205.187')
-#     print (geolocation .__dict__)
+aliquot_sum = sum([x for x in range(1,number) if number % x == 0])
+newList = [perfect.append(number) if aliquot_sum == number else abundant.append(number) if aliquot_sum > number else deficient.append(number) for number in range(29)]
+print (newList)
+print('================================================================================\n')
