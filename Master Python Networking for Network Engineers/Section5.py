7ba921fb9b533790510947af410155ed7b0d6dfb
Jeremy <jeremy.king029@gmail.com>
2021-05-26 14:37:34 -0400

    Automate

--- Master Python Networking for Network Engineers/Section5.py
+++ Master Python Networking for Network Engineers/Section5.py
@@ -5,8 +5,13 @@
 import paramiko 
 
 ssh_client = paramiko.SSHClient()
-print (type(ssh_client))0
+print (type(ssh_client))
 
 print ('Connecting to ')
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname='10.1.1.10', port='22', )
+
+# Video 55 Intro to Paramiko
+# pip install paramiko
+
+import paramiko
