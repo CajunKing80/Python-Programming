f132b947d07fb6c39beaf23c8b8cfb49c8c63f51
Jeremy <jeremy.king029@gmail.com>
2021-06-03 13:23:13 -0400

    Code Review

--- VS Code/Code Review/Code_Review_May_27.py
+++ VS Code/Code Review/Code_Review_May_27.py
@@ -3,16 +3,16 @@
 # =================================================================================================
 
 
-# print('\n===================================================== EASY =======================================================================')
-# alphabet = []
+print('\n===================================================== EASY =======================================================================')
+alphabet = []
 
-# for letter in 'abcdefghijklmnopqrstuvwxyz': 
-#     alphabet.append(letter)
-# print(alphabet)
+for letter in 'abcdefghijklmnopqrstuvwxyz': 
+    alphabet.append(letter)
+print(alphabet)
 
-# alphabet = [letter for letter in 'abcdefghijklmnopqrstuvwxyz']
-# print (alphabet)
-# print('==================================================================================================================================\n')
+alphabet = [letter for letter in 'abcdefghijklmnopqrstuvwxyz']
+print (alphabet)
+print('==================================================================================================================================\n')
 
 # =================================================================================================
 # MEDIUM ==========================================================================================
@@ -20,31 +20,31 @@
 
 # Conditional List Comprehension
 
-# print('\n======= MEDIUM: CONDITIONAL =======')
-# number = []
+print('\n======= MEDIUM: CONDITIONAL =======')
+number = []
 
-# for num in range(20):
-#     if num % 2 != 0:
-#         number.append(num)
-# print(number)
+for num in range(20):
+    if num % 2 != 0:
+        number.append(num)
+print(number)
 
-# number_list = [number for number in range(20) if number % 2 != 0]
-# print (number_list)
-# print('======= MEDIUM: CONDITIONAL =======\n')
+number_list = [number for number in range(20) if number % 2 != 0]
+print (number_list)
+print('======= MEDIUM: CONDITIONAL =======\n')
 
-# # Nested List Comprehension
+# Nested List Comprehension
 
-# print('\n============ MEDIUM: NESTED ============')
-# numbers = []
-# for number in range(100):
-#     if number % 2 == 0:
-#         if number % 5 == 0:
-#             numbers.append(number)
-# print (numbers) 
+print('\n============ MEDIUM: NESTED ============')
+numbers = []
+for number in range(100):
+    if number % 2 == 0:
+        if number % 5 == 0:
+            numbers.append(number)
+print (numbers) 
 
-# listOfNumbers = [number for number in range(100) if number % 2 == 0 if number % 5 == 0]
-# print (listOfNumbers)
-# print('============ MEDIUM: NESTED ============\n')
+listOfNumbers = [number for number in range(100) if number % 2 == 0 if number % 5 == 0]
+print (listOfNumbers)
+print('============ MEDIUM: NESTED ============\n')
 
 # =================================================================================================
 # HARD ============================================================================================
@@ -56,20 +56,20 @@ perfect = []
 abundant = []
 deficient = []
 
-# for number in range(29): 
+for number in range(29): 
        
-#     aliquot_sum = 0
+    aliquot_sum = 0
     
-#     for x in range(1,number):
-#         if (number % x == 0):
-#             aliquot_sum = aliquot_sum + x
+    for x in range(1,number):
+        if (number % x == 0):
+            aliquot_sum = aliquot_sum + x
 
-#     if aliquot_sum == number:
-#         perfect.append(number)
-#     elif aliquot_sum > number:
-#         abundant.append(number)
-#     elif aliquot_sum < number: 
-#         deficient.append(number)
+    if aliquot_sum == number:
+        perfect.append(number)
+    elif aliquot_sum > number:
+        abundant.append(number)
+    elif aliquot_sum < number: 
+        deficient.append(number)
 
 # print (f'Perfect   : {perfect} \nAbundant  : {abundant} \nDeficient : {deficient}')
 
