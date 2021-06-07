f842848f865f12d6a4916a016b931e0977a3a989
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-06-07 17:57:18 -0400

    dunder commits

--- VS Code/dunder_test.py
+++ VS Code/dunder_test.py
@@ -1,1 +1,6 @@
-print("Dunder Test __name__ is set to: {}" .format(__name__))
+
+def get_name():
+    print("Dunder Test __name__ is set to: {}" .format(__name__))
+
+if __name__ == '__main__':
+    get_name()
--- VS Code/dunder_test2.py
+++ VS Code/dunder_test2.py
@@ -1,3 +1,7 @@
-import dunder_test 
+from dunder_test import get_name
 
-print("Dunder Test 2 __name__ is set to: {}" .format(__name__))
+def get_name_2():
+    print("Dunder Test 2 __name__ is set to: {}" .format(__name__))
+
+if __name__ == '__main__':
+    get_name()
