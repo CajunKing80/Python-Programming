4ed7c9370d783f63e1ef0d53a62d13fe683fb78f
Jeremy <jeremy.king029@gmail.com>
2021-06-17 14:26:12 -0400

    Code Review

--- VS Code/Code Review/Code_Review_Jun_10.py
+++ VS Code/Code Review/Code_Review_Jun_10.py
@@ -44,6 +44,7 @@ if __name__ == "__main__":
 
 
 import requests
+from dnacentersdk import api
 
 headers = {
     'X-Auth-Token': 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MDJjMGUyODE0NzEwYTIyZDFmN2UxNzIiLCJhdXRoU291cmNlIjoiaW50ZXJuYWwiLCJ0ZW5hbnROYW1lIjoiVE5UMCIsInJvbGVzIjpbIjYwMmJlYmU1MTQ3MTBhMDBjOThmYTQwOSJdLCJ0ZW5hbnRJZCI6IjYwMmJlYmU1MTQ3MTBhMDBjOThmYTQwMiIsImV4cCI6MTYyMjg0MzU5MCwiaWF0IjoxNjIyODM5OTkwLCJqdGkiOiIyNDExZWFmYy01ODQ5LTQ5YWUtODIxYi05YzIyM2E4YWRjMDgiLCJ1c2VybmFtZSI6ImRldm5ldHVzZXIifQ.U6d33UroOiIRH4LQOBw7rYTitFvsx0TAvk0ubAnSWSsfjY9p7qDTqnKex5Wo_SjAym7-9_dqvucofWibA8qnJFxqHqGSU9SFOdcvBB-21uT5RKnfG9mQXEXeScHEfh13quEl6SjwhUFgiG9zG8HQokrwDcinom280Ty_2B1pxuVS6TGeBFptJ2qHCox-1nwJjIEqMKMmqPpmbkoYZIcFhYOxWm4s7O23p2qetLeMdrMguIk-xbtdz1O3dFMU4cP_MLcGffu0FSPKN35Xdt_lADAEhZym3fkpMeazfJBnPVzOHszoTVFNonl9Czpl_xX18CBD9GbhFatlLZCsqbht7w',
@@ -51,7 +52,6 @@ headers = {
 
 response = requests.get('https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device', headers=headers)
 
-from dnacentersdk import api
 
 DNAC = api.DNACenterAPI(username="devnetuser", 
             password="Cisco123!", 
