ee38df89217b329bec15a754ed4f604c572fe97d
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-06-17 19:50:13 -0400

    Network Programmability

--- Network Programmability and Automation/Chapter 5/Ch 5.py
+++ Network Programmability and Automation/Chapter 5/Ch 5.py
@@ -1,3 +1,11 @@
 ##############################################
 #######  Data Formats and Data Models  #######
 ##############################################
+
+import yaml
+
+with open ('example.yml') as f: 
+    result = yaml.load(f)
+    print (result)
+    type (result)
+    print ()    
