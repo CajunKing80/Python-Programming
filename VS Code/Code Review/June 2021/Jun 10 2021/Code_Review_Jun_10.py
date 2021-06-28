65500e1813df8467ff5a6ee5b3fa6c884c7a038a
Jeremy <jeremy.king029@gmail.com>
2021-06-28 15:01:31 -0400

    COde review changes

--- VS Code/Code Review/June 2021/Jun 10 2021/Code_Review_Jun_10.py
+++ VS Code/Code Review/June 2021/Jun 10 2021/Code_Review_Jun_10.py
@@ -11,10 +11,11 @@ from prettytable import PrettyTable
 
 
 def get_name():
-    print(f"\n{'='*100}  EASY  {'='*100}\n")
-    print("The name of my current program is set to: {}" .format(__name__))
-    print()
-    print('='*208)
+
+    print (f"\n{'='*100}  EASY  {'='*100}\n")
+    print ("The name of my current program is set to: {}" .format(__name__))
+    print ()
+    print ('='*208)
 
 if __name__ == '__main__':
     get_name()
@@ -28,12 +29,22 @@ if __name__ == '__main__':
 def get_auth_token():
     
     url = 'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token'
-    resp = requests.post(url, auth=HTTPBasicAuth(username='devnetuser', password='Cisco123!'))
+
+    resp = requests.post(
+        url, 
+        auth = HTTPBasicAuth(
+        username = 'devnetuser', 
+        password = 'Cisco123!'
+        )
+    )
+
     token = resp.json()['Token']
-    print(f"\n{'='*100} MEDIUM {'='*100}\n")
-    print("DNAC Token: {}".format(token))
-    print()
-    print('='*209)
+
+    print (f"\n{'='*100} MEDIUM {'='*100}\n")
+    print ("DNAC Token: {}".format(token))
+    print ()
+    print ('='*209)
+
     return token
     
 if __name__ == "__main__":
@@ -52,28 +63,38 @@ def get_network_devices():
         }
          
     url = 'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device'
-    response = requests.get(url, headers=headers)
+    
+    response = requests.get(
+        url,
+        headers = headers
+        )
 
 
-    DNAC = api.DNACenterAPI (
-                username="devnetuser", 
-                password="Cisco123!", 
-                base_url="https://sandboxdnac.cisco.com"
-                )
+    DNAC = api.DNACenterAPI(
+        username = "devnetuser", 
+        password = "Cisco123!", 
+        base_url = "https://sandboxdnac.cisco.com"
+        )
 
-    print(f"\n{'='*100}  HARD  {'='*100}\n")
+    print (f"\n{'='*100}  HARD  {'='*100}\n")
+
+    table = PrettyTable(
+        [
+            "Hostname", 
+            "Device Type", 
+            "Last Updated"
+            ]
+            )
 
     devices = DNAC.devices.get_device_list()
-
-    table = PrettyTable(["Hostname", "Device Type", "Last Updated"])
     
     for device in devices.response: 
 
         table.add_row([device["hostname"],device["deviceType"],device["lastUpdated"]])
     
     print (table)
-    print()
-    print('='*208)
+    print ()
+    print ('='*208)
 
 
 
