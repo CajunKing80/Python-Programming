5783bff855c65e6320e3489702f84fe4f76e58e9
Jeremy <jeremy.king029@gmail.com>
2021-06-28 11:11:19 -0400

    Code review

---
+++ VS Code/Code Review/July 2021/July 15 2021/Code_Review_July_15.py
@@ -0,0 +1,15 @@
+# Ansible
+
+# ansible --version
+#     Based on the file path of the ansible.cfg
+#     Create an inventory file within the same directory
+
+# touch inventory.txt
+
+# Create IOSXE network devices, and add CSR1 & CSR 2 to the inventory File
+
+# Create an SNMP playbook to push SNMP configs to both CSR's
+#     snmp-server community ansible-test RO 
+#     snmp-server community location Bragg
+#     snmp-server contact NOT_ANSIBLE
+
