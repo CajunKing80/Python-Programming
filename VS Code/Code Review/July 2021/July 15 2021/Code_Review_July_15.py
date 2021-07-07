eb14fc7873f6a7bc2509da3df3da7be729039e1c
Jeremy <jeremy.king029@gmail.com>
2021-07-07 09:30:20 -0400

    Jul 15 Code Review

--- VS Code/Code Review/July 2021/July 15 2021/Code_Review_July_15.py
+++ VS Code/Code Review/July 2021/July 15 2021/Code_Review_July_15.py
@@ -1,15 +1,39 @@
-# Ansible
+EASY
 
-# ansible --version
-#     Based on the file path of the ansible.cfg
-#     Create an inventory file within the same directory
+/etc/ansible
 
-# touch inventory.txt
+Create an Ansible inventory file with the following devices
 
-# Create IOSXE network devices, and add CSR1 & CSR 2 to the inventory File
+CSR1	{ ip address }
+CSR2	{ ip address }
+CSR3  	{ ip address }
 
-# Create an SNMP playbook to push SNMP configs to both CSR's
-#     snmp-server community ansible-test RO 
-#     snmp-server community location Bragg
-#     snmp-server contact NOT_ANSIBLE
+MEDIUM
 
+Modify the Ansible inventory file, and group the devices by device type. Also include 
+global variables to include the ssh username and password. (This assumes that all devices
+in the inventory are managed and configured with the same SSH username and password)
+
+[all:vars]
+ansible_user=ansible
+ansible_ssh_pass=ansible
+
+[iosxe]
+CSR1
+CSR2
+CSR3
+
+HARD
+
+Using an emulator of choice, build a basic Ansible lab. Lab should consist of 1 Ansible 
+controller and a minimum of 2 managed nodes. (examples include a Cisco virtual router, or
+secondary Linux machines with ip routing enabled)
+
+Configure an Ansible playbook that will configure the connected interface IP addresses
+into a running state; configure a static route between the devices, and save the running
+configuration to the startup config. 
+
+Execute the playbook. 
+
+Log into the remote devices and confirm the playbook performed the basic interface
+configuration, built the static route, and saved the configuration.  
