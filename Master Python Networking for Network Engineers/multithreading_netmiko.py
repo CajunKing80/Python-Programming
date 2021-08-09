3e91029033d4fdf6c4d8d37a251cdfb4a8af3687
Jeremy <jeremy.king029@gmail.com>
2021-08-09 08:42:20 -0400

    Black multithreading_netmiko

--- Master Python Networking for Network Engineers/multithreading_netmiko.py
+++ Master Python Networking for Network Engineers/multithreading_netmiko.py
@@ -1,45 +1,47 @@
 from netmiko import ConnectHandler
 from datetime import datetime
-import time 
+import time
 import threading
 
 start = time.time()
 
+
 def backup(device):
     connection = ConnectHandler(**device)
-    print (f"Connecting To Router {ip}")
+    print(f"Connecting To Router {ip}")
     connection.enable()
 
-    output = connection.send_command('show run')
+    output = connection.send_command("show run")
     prompt = connection.find_prompt()
-    hostname = prompt [0:-1]
+    hostname = prompt[0:-1]
 
     now = datetime.now()
     year = now.year
-    month = now.month 
+    month = now.month
     day = now.day
 
-    filename = f'{hostname}_{year}--{month}--{day}_backup.txt'
-    with open (filename, 'w') as backup:
+    filename = f"{hostname}_{year}--{month}--{day}_backup.txt"
+    with open(filename, "w") as backup:
         backup.write(output)
-        print (f"Backup of {hostname} successful")
+        print(f"Backup of {hostname} successful")
 
-    print (f"Closing connection to {hostname}")
+    print(f"Closing connection to {hostname}")
     connection.disconnect()
 
-with open ('devices.txt') as f:
+
+with open("devices.txt") as f:
     devices = f.read().splitlines()
 
 threads = list()
-for ip in devices: 
+for ip in devices:
 
     device = {
-        'device_type': 'cisco_ios',
-        'host': ip,
-        'username': 'ansible', 
-        'password': 'ansible', 
-        'port': 22,
-        'verbose': True
+        "device_type": "cisco_ios",
+        "host": ip,
+        "username": "ansible",
+        "password": "ansible",
+        "port": 22,
+        "verbose": True,
     }
 
     th = threading.Thread(target=backup, args=(device,))
@@ -52,4 +54,4 @@ for th in threads:
     th.join()
 
 end = time.time()
-print (f"Total execution time: {end - start}")
+print(f"Total execution time: {end - start}")
