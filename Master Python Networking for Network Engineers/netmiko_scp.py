66ddc7a4aead3efd5fa4f077abe81f85ef01660e
Jeremy <jeremy.king029@gmail.com>
2021-08-10 12:48:02 -0400

    netmiko_scp updates

--- Master Python Networking for Network Engineers/netmiko_scp.py
+++ Master Python Networking for Network Engineers/netmiko_scp.py
@@ -3,9 +3,9 @@ from netmiko import file_transfer
 
 device = {
     "device_type": "cisco_ios",
-    "host": "192.168.0.152",
-    "username": "ansible",
-    "password": "ansible",
+    "host": "10.211.211.1",
+    "username": "commadmin",
+    "password": "password",
     "port": 22,
     "verbose": True
 }
@@ -15,10 +15,11 @@ connection = ConnectHandler(**device)
 transfer_output = file_transfer(
     connection,
     source_file="csr1000v-universalk9.17.03.03.SPA.bin",
-    dest_file="csr1000v-universalk9.17.03.03.SPA.bin",
+    dest_file="csr1000v-universalk9.17.03.03.SPA.bin69",
     file_system="flash:",
     direction="put",
-    overwrite_file=True
+    overwrite_file=True,
+    socket_timeout=30,
 )
 
 print(transfer_output)
